34681b8ba879167d910ea25260a90128d8e49dfb
Vinay Kumar Mogulluru <vinayeim@gmail.com>
2017-03-20 09:37:36 +0530

    1

---
+++ R/Scripts/0 R Tutorial/0 Basic Syntax.R
@@ -0,0 +1,10 @@
+
+
+
+# All the variables can be deleted by using the rm() and ls() function together.
+
+print(ls())
+'or'
+ls()
+
+rm(list = ls())
---
+++ R/Scripts/0 R Tutorial/2 Installation and Setup.R
@@ -0,0 +1,33 @@
+# INSTALLATION AND SETUP#####
+
+#R Base Software
+# Goto the 'https://cran.r-project.org/' Download the R-Base software.
+
+#R Studio
+# Goto the 'https://www.rstudio.com/' Download the RStudio software.
+
+#R Toos software
+# Goto the 'https://cran.r-project.org/bin/windows/Rtools/' Download the Rtools software.
+
+#	Installing Packages#####
+
+#Source of R Packages
+#CRAN
+
+"https://cran.r-project.org/"
+
+#GitHub
+"github.com"
+
+#Bioconductor
+
+"https://bioconductor.org/packages/"
+
+
+1 + 1
+print("Hello World")
+5 / 2 * 3
+10 %% 3  # get the remainder 
+10 %/% 3  # get the quotient
+
+#?print
